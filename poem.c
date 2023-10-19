4dfc9711b9564ecab1cc6b161f1b2ed40f2cb53f
Anil Sapkota <45574896+anilsapkota@users.noreply.github.com>
2023-10-19 13:09:22 -0500

    Basic printf statements in C

---
+++ poem.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void){
+
+    printf("\nThe Road Not Taken  -- Robert Frost \n\n");
+    printf("Two roads diverged in a yellow wood,\n");
+    printf("And sorry I could not travel both \n");
+    printf("And be one traveler,long I stood \n");
+    printf("And looked down one as far as I could \n");
+    printf("To where it bent in the undergrowth; \n\n");
+
+    printf("Then took the other,as just as fair,\n");
+    printf("And having perhaps the better claim,\n");
+    printf("Because it was grassy and wanted wear;\n");
+    printf("Though as for that the passing there\n");
+    printf("Had worn them really about the same,\n\n");
+    return 0;
+}
